db72e81c7231b933d8bb487063b5f185f03ad743
Segun <lawalsegun72@gmail.com>
2023-01-22 09:37:45 +0100

    style: refactor navbar

--- src/components/TopNav.tsx
+++ src/components/TopNav.tsx
@@ -1,166 +1,114 @@
-import "../App.css";
-import { useState, useEffect } from "react";
-import ClickAwayListener from "@mui/base/ClickAwayListener";
+import { useState, useRef } from "react";
 import { NavLink } from "react-router-dom";
-import {
-  Navbar,
-  MobileNav,
-  Typography,
-  IconButton,
-} from "@material-tailwind/react";
 import logo from "../assets/habitter.png";
+import "../../src/App.css";
 
-export default function Example() {
-  const [openNav, setOpenNav] = useState(false);
-
-  useEffect(() => {
-    window.addEventListener(
-      "resize",
-      () => window.innerWidth >= 960 && setOpenNav(false)
-    );
-  }, []);
-
-  const navList = (
-    <ul className="mb-4 mt-2 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-14">
-      <Typography
-        as="li"
-        variant="h6"
-        color="blue-gray"
-        className="p-1 nav-items font-normal"
-      >
-        <NavLink
-          reloadDocument
-          to="/blog"
-          className={({ isActive }) =>
-            isActive
-              ? " flex items-center text-[#217BF4] font-bold"
-              : "r-black hover:text-[#217BF4] flex items-center"
-          }
-        >
-          Blog
-        </NavLink>
-        {/* <a href="/about">ABOUT</a> */}
-      </Typography>
-      <Typography
-        as="li"
-        variant="h6"
-        color="blue-gray"
-        className="p-1 nav-items font-normal"
-      >
-        <NavLink
-          reloadDocument
-          to="/signin"
-          className={({ isActive }) =>
-            isActive
-              ? " flex items-center text-[#217BF4] font-bold"
-              : "r-black hover:text-[#217BF4] flex items-center"
-          }
-        >
-          Sign In
-        </NavLink>
-        {/* <a href="/services">SERVICES</a> */}
-      </Typography>
-      <Typography
-        as="li"
-        variant="h6"
-        color="blue-gray"
-        className="p-1 nav-items font-normal"
-      >
-        <NavLink
-          reloadDocument
-          to="/signup"
-          className={({ isActive }) =>
-            isActive
-              ? " flex items-center text-[#217BF4] font-bold"
-              : "r-black hover:text-[#217BF4] flex items-center"
-          }
-        >
-          Sign Up
-        </NavLink>
-        {/* <a href="/portfolio">PORTFOLIO</a> */}
-      </Typography>
-      <Typography
-        as="li"
-        variant="h6"
-        color="blue-gray"
-        className="p-1 nav-items font-normal"
-      >
-        {/* <NavLink
-          reloadDocument
-          to="/contact"
-          className={({ isActive }) =>
-            isActive
-              ? " flex items-center text-blue-400 font-bold"
-              : "r-black hover:text-blue-400 flex items-center"
-          }
-        >
-          CONTACT
-        </NavLink> */}
-        {/* <a href="/contact">CONTACT</a> */}
-      </Typography>
-    </ul>
-  );
-  const handleClickAway = () => {
-    setOpenNav(false);
-  };
+// eslint-disable-next-line no-unused-vars
+export default function NavBar() {
+  const navMenu = useRef(null);
+  const [navbar, setNavbar] = useState(false);
 
   return (
-    <ClickAwayListener onClickAway={handleClickAway}>
-      <Navbar className="text-black navbar max-w-none py-2 px-4 lg:px-8 lg:py-4 ">
-        <div className="container mx-auto flex items-center justify-between text-blue-gray-900 border-b-[1px] border-[#217BF4] border-opacity-[0.2]">
-          <Typography
-            as="a"
-            href="/"
-            variant="small"
-            className="mr-4 flex cursor-pointer py-1.5 font-normal"
-          >
-            <span>
-              <img className="logo w-[50px]" src={logo} alt="habitter logo" />
-            </span>
-            <span className="logo-name font-bold text-3xl">Habitter</span>
-          </Typography>
-          <div className="hidden lg:block">{navList}</div>
-
-          <IconButton
-            variant="text"
-            className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent xl:hidden"
-            ripple={false}
-            onClick={() => setOpenNav(!openNav)}
-          >
-            {openNav ? (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                className="h-7 w-7"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                strokeWidth={3}
+    <nav
+      ref={navMenu}
+      className="py-8 mx-8 border-b-[1px] border-[#217BF4] border-opacity-[0.2]"
+    >
+      <div className="justify-between mx-auto lg:items-center lg:flex">
+        <div>
+          <div className="flex items-center justify-between lg:block">
+            <NavLink to="/" className="flex gap-[6px]">
+              <img src={logo} alt="habitter logo" className="w-[36px]" />
+              <span className="font-semibold text-[1.5rem]">Habitter</span>
+            </NavLink>
+            <div className="lg:hidden">
+              <button
+                type="submit"
+                className="p-2 rounded-md outline-none"
+                onClick={() => setNavbar(!navbar)}
               >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M6 18L18 6M6 6l12 12"
-                />
-              </svg>
-            ) : (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-7 w-7"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth={3}
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M4 6h16M4 12h16M4 18h16"
-                />
-              </svg>
-            )}
-          </IconButton>
+                {navbar ? (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="w-9 h-9 text-white"
+                    viewBox="0 0 20 20"
+                    fill="#434343"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                ) : (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="w-9 h-9 text-white"
+                    fill="black"
+                    viewBox="0 0 24 24"
+                    stroke="#434343"
+                    strokeWidth={2}
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M4 6h16M4 12h16M4 18h16"
+                    />
+                  </svg>
+                )}
+              </button>
+            </div>
+          </div>
         </div>
-        <MobileNav open={openNav}>{navList}</MobileNav>
-      </Navbar>
-    </ClickAwayListener>
+        <div>
+          <div
+            className={`flex-1 justify-self-center pb-3 mt-8 lg:block md:pb-0 lg:mt-0 ${
+              navbar ? "block" : "hidden"
+            }`}
+          >
+            <ul className="bg- lg:bg-inherit items-center xl:gap-12 justify-center space-y-5 lg:flex lg:space-x-6 lg:space-y-0">
+              <li className="leading-[25px]">
+                <NavLink
+                  to="/blog"
+                  reloadDocument
+                  className={({ isActive }) =>
+                    isActive
+                      ? " flex items-center text-[#217BF4] font-bold"
+                      : "hover:text-[#217BF4] flex items-center"
+                  }
+                >
+                  Blog
+                </NavLink>
+              </li>
+              <li className="leading-[25px]">
+                <NavLink
+                  reloadDocument
+                  to="/sign-up"
+                  className={({ isActive }) =>
+                    isActive
+                      ? " flex items-center text-[#217BF4] font-bold"
+                      : "hover:text-[#217BF4] flex items-center"
+                  }
+                >
+                  Sign Up
+                </NavLink>
+              </li>
+              <li className="leading-[25px]">
+                <NavLink
+                  reloadDocument
+                  to="/sign-in"
+                  className={({ isActive }) =>
+                    isActive
+                      ? " flex items-center text-[#217BF4] font-bold"
+                      : "hover:text-[#217BF4] flex items-center"
+                  }
+                >
+                  Sign In
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </nav>
   );
 }
