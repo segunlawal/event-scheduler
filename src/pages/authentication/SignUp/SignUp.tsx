a44f46c8a8557299c1555f5816a1170f5ed4df91
Segun <lawalsegun72@gmail.com>
2023-01-25 22:30:25 +0100

    style: style signup page

--- src/pages/authentication/SignUp/SignUp.tsx
+++ src/pages/authentication/SignUp/SignUp.tsx
@@ -1,14 +1,11 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { Dispatch, SetStateAction, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import { createUserWithEmailAndPassword, User } from "firebase/auth";
 import { auth } from "../../../firebase-config";
 import "../../../App.css";
 import logo from "../../../assets/habitter.png";
-// interface SignUpProps {
-//   user: User | null;
-//   setUser: Dispatch<SetStateAction<User | null>>;
-// }
+import { FcGoogle } from "react-icons/fc";
 function SignUp(): JSX.Element {
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
@@ -36,22 +33,24 @@ function SignUp(): JSX.Element {
   // };
 
   return (
-    <div className="onboard min-h-screen flex flex-col">
-      <div className="flex justify-center gap-1">
-        <img src={logo} alt="habitter logo" className="w-[36px] h-[36px]" />
-        <p className="font-semibold text-3xl">Habitter</p>
-      </div>
-      <p className="text-2xl mx-auto font-semibold tracking-tight">
-        Create an account to start tracking your habits
+    <div className="onboard min-h-screen flex flex-col py-20">
+      <Link to="/">
+        <div className="flex justify-center gap-1">
+          <img src={logo} alt="habitter logo" className="w-[36px] h-[36px]" />
+          <p className="font-semibold text-3xl">Habitter</p>
+        </div>
+      </Link>
+      <p className="text-3xl mx-auto font-semibold tracking-tight py-7">
+        Create an account
       </p>
       <div className="flex flex-col mx-auto">
-        <label htmlFor="first-name" className="text-sm">
+        <label htmlFor="first-name" className="text-sm pb-1">
           First Name
         </label>
         <input
           type="text"
           name="first-name"
-          className="border-2 rounded-lg p-2 w-96 bg-transparent border-[#217BF4]  focus:outline-none"
+          className="focus:border-2 border-[1px] rounded-lg p-3 mb-5 sm:w-[30rem] w-80 bg-transparent border-[#2b2b39] focus:outline-none"
           onChange={(event) => {
             setFirstName(event.target.value);
           }}
@@ -59,13 +58,13 @@ function SignUp(): JSX.Element {
         />
       </div>
       <div className="flex flex-col mx-auto">
-        <label htmlFor="last-name" className="text-sm">
+        <label htmlFor="last-name" className="text-sm pb-1">
           Last Name
         </label>
         <input
           type="text"
           name="last-name"
-          className="border-[1px] rounded-md p-2 w-96"
+          className="focus:border-2 border-[1px] rounded-lg p-3 mb-5 sm:w-[30rem] w-80 bg-transparent border-[#2b2b39] focus:outline-none"
           onChange={(event) => {
             setLastName(event.target.value);
           }}
@@ -73,13 +72,13 @@ function SignUp(): JSX.Element {
         />
       </div>
       <div className="flex flex-col mx-auto">
-        <label htmlFor="last-name" className="text-sm">
+        <label htmlFor="last-name" className="text-sm pb-1">
           Email
         </label>
         <input
           type="email"
           name="last-name"
-          className="border-[1px] rounded-md p-2 w-96"
+          className="focus:border-2 border-[1px] rounded-lg p-3 mb-5 sm:w-[30rem] w-80 bg-transparent border-[#2b2b39] focus:outline-none"
           onChange={(event) => {
             setRegisterEmail(event.target.value);
           }}
@@ -88,31 +87,41 @@ function SignUp(): JSX.Element {
       </div>
 
       <div className="flex flex-col mx-auto">
-        <label htmlFor="password" className="text-sm">
+        <label htmlFor="password" className="text-sm pb-1">
           Password
         </label>
         <input
           type="password"
           name="password"
-          className="border-[1px] rounded-md p-2 w-96"
+          className="focus:border-2 border-[1px] rounded-lg p-3 mb-5 sm:w-[30rem] w-80 bg-transparent border-[#2b2b39] focus:outline-none"
           onChange={(event) => {
             setRegisterPassword(event.target.value);
           }}
-          placeholder="Password"
+          placeholder="Enter password"
         />
       </div>
       <button
         type="submit"
-        className="text-white px-10 py-2 bg-[#217BF4]"
+        className="text-white text-md font-light px-10 py-2 bg-[#217BF4] sm:w-[30rem] w-80 mx-auto rounded-lg mt-3"
         // eslint-disable-next-line @typescript-eslint/no-misused-promises
         onClick={signUp}
       >
         Create Account
       </button>
-      <p>OR</p>
-      <button type="submit" className="px-10 border-2 py-2 border-[#217BF4]">
-        Sign Up using Google
+      <p className="mx-auto py-1">OR</p>
+      <button
+        type="submit"
+        className="px-10 border-2 py-2 border-[#217BF4] sm:w-[30rem] w-80 mx-auto rounded-lg flex gap-2 justify-center"
+      >
+        <FcGoogle className="flex my-auto" />
+        <p>Sign up with Google</p>
       </button>
+      <p className="mx-auto py-3">
+        Already have an account?{" "}
+        <Link to="/signin">
+          <span className="text-[#217BF4] tracking-tighter">Log In</span>
+        </Link>
+      </p>
     </div>
   );
 }
