dd88dd4c22f50116bf30d6bc3310012c8febbacb
Segun <lawalsegun72@gmail.com>
2023-01-25 05:31:52 +0100

    feat: enable user to log out

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,17 +1,33 @@
 import TopNav from "../../components/TopNav";
 import { Dispatch, SetStateAction } from "react";
-import { User } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
+import { User, signOut } from "firebase/auth";
+import { auth } from "../../firebase-config";
 
 interface DashboardProps {
   user: User | null;
   setUser: Dispatch<SetStateAction<User | null>>;
 }
 const Dashboard: React.FC<DashboardProps> = (user, setUser) => {
+  const navigate = useNavigate();
+
+  const logOut = async (): Promise<void> => {
+    //
+    await signOut(auth);
+    navigate("/");
+  };
   return (
     <div className="">
       <TopNav />
       <p className="text-xl">Welcome to your Dashboard</p>
       {user?.user?.email}
+      <button
+        type="submit"
+        // eslint-disable-next-line @typescript-eslint/no-misused-promises
+        onClick={logOut}
+      >
+        Log out
+      </button>
     </div>
   );
 };
