8f3a2ff452ef878a3406e4d077b0f1d71cff98ad
Segun <lawalsegun72@gmail.com>
2023-01-28 20:06:31 +0100

    feat: Add formik and yup to habdle validation in sign up page

--- src/pages/authentication/SignUp/SignUp.tsx
+++ src/pages/authentication/SignUp/SignUp.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../../../firebase-config";
@@ -10,14 +10,18 @@ import { FcGoogle } from "react-icons/fc";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { UserAuth } from "../../../context/AuthContext";
-
+import { Formik, Form, Field, ErrorMessage, FormikHelpers } from "formik";
+import * as Yup from "yup";
+interface FormValues {
+  registerEmail: string;
+  registerPassword: string;
+}
 function SignUp(): JSX.Element {
-  const [registerEmail, setRegisterEmail] = useState("");
-  const [registerPassword, setRegisterPassword] = useState("");
   const navigate = useNavigate();
   const { oneUser, googleSignIn } = UserAuth();
 
-  const signUp = async (): Promise<void> => {
+  const signUp = async (values: FormValues): Promise<void> => {
+    const { registerEmail, registerPassword } = values;
     try {
       const user = await createUserWithEmailAndPassword(
         auth,
@@ -48,6 +52,16 @@ function SignUp(): JSX.Element {
     }
   }, [oneUser]);
 
+  const SignUpSchema = Yup.object().shape({
+    registerEmail: Yup.string()
+      .email("Invalid email")
+      .required("Please enter your email address"),
+    registerPassword: Yup.string().required("Password is required"),
+    confirmPassword: Yup.string()
+      .oneOf([Yup.ref("registerPassword"), null], "Passwords must match")
+      .required("Please confirm your password"),
+  });
+
   return (
     <div className="onboard min-h-screen flex flex-col py-20">
       <ToastContainer />
@@ -61,42 +75,71 @@ function SignUp(): JSX.Element {
         Create an account
       </p>
 
-      <div className="flex flex-col mx-auto">
-        <label htmlFor="last-name" className="text-sm pb-1">
-          Email
-        </label>
-        <input
-          type="email"
-          name="last-name"
-          className="focus:border-2 border-[1px] rounded-lg p-3 mb-5 sm:w-[30rem] w-80 bg-transparent border-[#2b2b39] focus:outline-none"
-          onChange={(event) => {
-            setRegisterEmail(event.target.value);
-          }}
-          placeholder="Email"
-        />
-      </div>
-
-      <div className="flex flex-col mx-auto">
-        <label htmlFor="password" className="text-sm pb-1">
-          Password
-        </label>
-        <input
-          type="password"
-          name="password"
-          className="focus:border-2 border-[1px] rounded-lg p-3 mb-5 sm:w-[30rem] w-80 bg-transparent border-[#2b2b39] focus:outline-none"
-          onChange={(event) => {
-            setRegisterPassword(event.target.value);
-          }}
-          placeholder="Enter password"
-        />
-      </div>
-      <button
-        type="submit"
-        className="text-white text-md font-light px-10 py-2 bg-[#217BF4] sm:w-[30rem] w-80 mx-auto rounded-lg mt-3"
-        onClick={signUp}
+      <Formik
+        initialValues={{
+          registerEmail: "",
+          registerPassword: "",
+          confirmPassword: "",
+        }}
+        validationSchema={SignUpSchema}
+        onSubmit={signUp}
       >
-        Create Account
-      </button>
+        <Form className="flex flex-col mx-auto">
+          <div className="flex flex-col mx-auto">
+            <label htmlFor="registerEmail" className="text-sm pb-1">
+              Email
+            </label>
+            <Field
+              name="registerEmail"
+              className="focus:border-2 border-[1px] rounded-lg p-3 sm:w-[30rem] w-80 bg-transparent border-[#2b2b39] focus:outline-none"
+              placeholder="Email"
+            />
+            <ErrorMessage
+              name="registerEmail"
+              component="div"
+              className="text-red-700"
+            />
+          </div>
+          <div className="flex flex-col mx-auto">
+            <label htmlFor="registerPassword" className="text-sm pb-1 mt-5">
+              Password
+            </label>
+            <Field
+              name="registerPassword"
+              className="focus:border-2 border-[1px] rounded-lg p-3 sm:w-[30rem] w-80 bg-transparent border-[#2b2b39] focus:outline-none"
+              placeholder="Enter password"
+            />
+            <ErrorMessage
+              name="registerPassword"
+              component="div"
+              className="text-red-700"
+            />
+          </div>
+          <div className="flex flex-col mx-auto">
+            <label htmlFor="confirmPassword" className="text-sm mt-5 pb-1">
+              Confirm Password
+            </label>
+            <Field
+              name="confirmPassword"
+              className="focus:border-2 border-[1px] rounded-lg p-3 mb-5 sm:w-[30rem] w-80 bg-transparent border-[#2b2b39] focus:outline-none"
+              placeholder="Confirm password"
+            />
+            <ErrorMessage
+              name="confirmPassword"
+              component="div"
+              className="text-red-700"
+            />
+          </div>
+
+          <button
+            type="submit"
+            className="text-white text-md font-light px-10 py-2 bg-[#217BF4] sm:w-[30rem] w-80 mx-auto rounded-lg mt-3 disabled:opacity-50 transition-all duration-300"
+            // onClick={signUp}
+          >
+            Create Account
+          </button>
+        </Form>
+      </Formik>
       <p className="mx-auto py-1">OR</p>
       <button
         type="submit"
@@ -106,6 +149,7 @@ function SignUp(): JSX.Element {
         <FcGoogle className="flex my-auto text-2xl" />
         <p>Sign up with Google</p>
       </button>
+
       <p className="mx-auto py-3">
         Already have an account?{" "}
         <Link to="/signin">
