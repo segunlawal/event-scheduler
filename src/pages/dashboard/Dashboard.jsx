5d8a4dd85d9e13566547b834addb0dd944d36baf
Segun <lawalsegun72@gmail.com>
2023-02-02 03:43:53 +0100

    feat: user can create habit

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -2,7 +2,7 @@ import TopNav from "../../components/TopNav";
 import { useNavigate } from "react-router-dom";
 import { signOut } from "firebase/auth";
 import { auth, db } from "../../firebase-config";
-import { getDocs, collection } from "firebase/firestore";
+import { getDocs, collection, addDoc } from "firebase/firestore";
 import { UserAuth } from "../../context/AuthContext";
 import { useEffect, useState } from "react";
 import ClipLoader from "react-spinners/ClipLoader";
@@ -12,24 +12,26 @@ import "react-toastify/dist/ReactToastify.css";
 function Dashboard() {
   const navigate = useNavigate();
   const [loading, setLoading] = useState(true);
-  // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
   const [habits, setHabits] = useState([]);
+  const [newHabitName, setNewHabitName] = useState("");
+  const [newHabitDesc, setNewHabitDesc] = useState("");
+  const [newHabitDuration, setNewHabitDuration] = useState(0);
 
   const habitsRef = collection(db, "habits");
+  const getHabits = async () => {
+    const data = await getDocs(habitsRef);
+    const filteredData = data.docs.map((doc) => ({
+      ...doc.data(),
+      id: doc.id,
+    }));
+    try {
+      // console.log(filteredData);
+      setHabits(filteredData);
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
   useEffect(() => {
-    const getHabits = async () => {
-      const data = await getDocs(habitsRef);
-      const filteredData = data.docs.map((doc) => ({
-        ...doc.data(),
-        id: doc.id,
-      }));
-      try {
-        // console.log(filteredData);
-        setHabits(filteredData);
-      } catch (error) {
-        toast.error(error.message);
-      }
-    };
     getHabits();
   }, []);
 
@@ -49,10 +51,22 @@ function Dashboard() {
     return (
       <div key={habit.id}>
         <p>{habit.habitName}</p>
-        {/* <p>{habit.goalInDays}</p> */}
       </div>
     );
   });
+
+  const handleNewHabit = async () => {
+    try {
+      await addDoc(habitsRef, {
+        habitName: newHabitName,
+        habitDescription: newHabitDesc,
+        numberOfDays: newHabitDuration,
+      });
+      getHabits();
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
 
   return (
     <div className="">
@@ -71,6 +85,31 @@ function Dashboard() {
           </button>
           <p className="text-2xl">A list of my habits</p>
           {eachHabit}
+          <div className="flex flex-col mx-auto w-96 gap-5">
+            <input
+              placeholder="Enter a new habit"
+              className="border-2"
+              onChange={(e) => setNewHabitName(e.target.value)}
+            />
+            <textarea
+              placeholder="describe habit"
+              className="border-2"
+              onChange={(e) => setNewHabitDesc(e.target.value)}
+            />
+            <input
+              placeholder="duration"
+              type="number"
+              className="border-2"
+              onChange={(e) => setNewHabitDuration(Number(e.target.value))}
+            />
+            <button
+              type="submit"
+              className="border-2 bg-blue-300"
+              onClick={handleNewHabit}
+            >
+              Add Habit
+            </button>
+          </div>
         </>
       )}
     </div>
