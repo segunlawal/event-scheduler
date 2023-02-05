9cb29cca65dfd9085b320b61fab89f7dbf2935f1
Segun <lawalsegun72@gmail.com>
2023-02-05 06:54:04 +0100

    feat: add modal for creating a new habit

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -9,6 +9,7 @@ import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import logo from "../../assets/habitter.png";
 import { IoMdArrowDropdown } from "react-icons/io";
+import Modal from "react-modal";
 
 function Dashboard() {
   const navigate = useNavigate();
@@ -17,7 +18,7 @@ function Dashboard() {
   const [newHabitName, setNewHabitName] = useState("");
   const [newHabitDesc, setNewHabitDesc] = useState("");
   const [newHabitDuration, setNewHabitDuration] = useState(0);
-  // eslint-disable-next-line no-unused-vars
+  const [modalIsOpen, setModalIsOpen] = useState(false);
   const [showDropDown, setShowDropDown] = useState(false);
 
   const habitsRef = collection(db, "habits");
@@ -80,25 +81,79 @@ function Dashboard() {
         </div>
       ) : (
         <div className="">
+          <Modal
+            style={{
+              overlay: {
+                position: "fixed",
+                background: "rgba(24, 49, 64, 0.63)",
+                backdropFilter: 'blur("91px")',
+              },
+            }}
+            isOpen={modalIsOpen}
+            className="bg-white flex flex-col mt-[10%] py-10 sm:w-[40%] w-[90%] mx-auto justify-center items-center"
+            appElement={document.getElementById("root") || undefined}
+            onRequestClose={() => {
+              setModalIsOpen(false);
+            }}
+          >
+            <div className="flex flex-col mx-auto w-full px-10 gap-3">
+              <p className="text-xl">Create a new habit</p>
+              <div className="flex flex-col">
+                <label>Habit Name</label>
+                <input
+                  placeholder="Enter a new habit"
+                  className="border-[1px] border-[#2b2b39] p-2 rounded-sm"
+                  onChange={(e) => setNewHabitName(e.target.value)}
+                />
+              </div>
+              <div className="flex flex-col">
+                <label>Habit Description</label>
+                <textarea
+                  placeholder="Briefly describe habit"
+                  className="border-[1px] border-[#2b2b39] p-2 rounded-sm"
+                  onChange={(e) => setNewHabitDesc(e.target.value)}
+                />
+              </div>
+              <div className="flex flex-col">
+                <label>Duration (in days)</label>
+                <input
+                  placeholder="Enter timeline in days"
+                  type="number"
+                  className="border-[1px] border-[#2b2b39] p-2 rounded-sm"
+                  onChange={(e) => setNewHabitDuration(Number(e.target.value))}
+                />
+              </div>
+              <button
+                type="submit"
+                className="border-2 bg-[#217BF4] text-white w-20 border-none rounded-md py-1"
+                onClick={handleNewHabit}
+              >
+                Add
+              </button>
+            </div>
+          </Modal>
           <div className="p-5 flex justify-between">
             <NavLink to="/" className="flex gap-[6px]">
               <img src={logo} alt="habitter logo" className="w-8 h-8" />
-              <span className="font-semibold text-[1.5rem]">Habitter</span>
+              <span className="hidden sm:block font-semibold text-[1.5rem]">
+                Habitter
+              </span>
             </NavLink>
-            <div>
-              <div
-                className="flex cursor-pointer"
-                onClick={() => setShowDropDown(!showDropDown)}
-              >
+            <div
+              onMouseEnter={() => setShowDropDown(true)}
+              onMouseLeave={() => setShowDropDown(false)}
+            >
+              <div className="flex cursor-pointer">
                 <p>{oneUser?.email}</p>
                 <IoMdArrowDropdown className="text-2xl cursor-pointer" />
               </div>
               {showDropDown && (
-                <div className="absolute flex mx-auto shadow-2xl border-[2px] border-[#217BF4] rounded-lg">
+                <div className="drop-down absolute flex mx-auto shadow-2xl hover:bg-[#217BF4] hover:text-white">
                   <button
                     type="submit"
                     onClick={logOut}
-                    className="flex mx-auto"
+                    className=""
+                    style={{ width: `${oneUser?.email.length}ch` }}
                   >
                     Log out
                   </button>
@@ -106,34 +161,14 @@ function Dashboard() {
               )}
             </div>
           </div>
-          <p className="text-xl mt-3">Welcome to your Dashboard</p>
+          <button
+            className="rounded-md text-white bg-[#217BF4] p-2 mx-auto flex"
+            onClick={() => setModalIsOpen(true)}
+          >
+            Add a new habit
+          </button>
           <p className="text-2xl">A list of my habits</p>
           {eachHabit}
-          <div className="flex flex-col mx-auto w-96 gap-5">
-            <input
-              placeholder="Enter a new habit"
-              className="border-2"
-              onChange={(e) => setNewHabitName(e.target.value)}
-            />
-            <textarea
-              placeholder="describe habit"
-              className="border-2"
-              onChange={(e) => setNewHabitDesc(e.target.value)}
-            />
-            <input
-              placeholder="duration"
-              type="number"
-              className="border-2"
-              onChange={(e) => setNewHabitDuration(Number(e.target.value))}
-            />
-            <button
-              type="submit"
-              className="border-2 bg-blue-400"
-              onClick={handleNewHabit}
-            >
-              Add Habit
-            </button>
-          </div>
         </div>
       )}
     </div>
